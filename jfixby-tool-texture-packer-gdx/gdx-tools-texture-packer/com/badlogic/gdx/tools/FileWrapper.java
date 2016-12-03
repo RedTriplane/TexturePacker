061d26d0c88b2582a934750a5f9f65944ece1816
J Fixby <github@jfixby.com>
2016-12-03 21:27:40 +0100

    fix errors reporting

--- jfixby-tool-texture-packer-gdx/gdx-tools-texture-packer/com/badlogic/gdx/tools/FileWrapper.java
+++ jfixby-tool-texture-packer-gdx/gdx-tools-texture-packer/com/badlogic/gdx/tools/FileWrapper.java
@@ -1,36 +1,39 @@
+
 package com.badlogic.gdx.tools;
 
 import com.badlogic.gdx.files.FileHandle;
 import com.jfixby.cmns.adopted.gdx.fs.ToGdxFileAdaptor;
+import com.jfixby.cmns.api.err.Err;
 import com.jfixby.cmns.api.file.File;
 import com.jfixby.cmns.api.file.LocalFileSystem;
 import com.jfixby.cmns.api.log.L;
 
 public class FileWrapper {
 
-	public static File file(String path) {
+	public static File file (final String path) {
 
-		java.io.File f = new java.io.File(path);
+		final java.io.File f = new java.io.File(path);
 		L.d("File", f.getAbsolutePath());
 		//
-		File file = LocalFileSystem.newFile(path);
+		final File file = LocalFileSystem.newFile(path);
 
 		L.d("File >>> ", file);
-		throw new Error();
+		Err.reportNotImplementedYet();
+		return file;
 
 		// return;
 	}
 
-	public static File file(File root, String string) {
+	public static File file (final File root, final String string) {
 		return root.child(string);
 	}
 
-	public static File file(File input) {
+	public static File file (final File input) {
 		return input;
 	}
 
-	public static FileHandle newFileHandle(File outputFile) {
-		ToGdxFileAdaptor gdx_file = new ToGdxFileAdaptor(outputFile);
+	public static FileHandle newFileHandle (final File outputFile) {
+		final ToGdxFileAdaptor gdx_file = new ToGdxFileAdaptor(outputFile);
 		return gdx_file;
 	}
 
